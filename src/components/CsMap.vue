94c4c7f9843295140c6ba26ec4bb6220e5be4dca
Michal Zimmermann <zimmicz@gmail.com>
2019-02-17 17:06:53 +0100

    Use vue2leaflet

--- src/components/CsMap.vue
+++ src/components/CsMap.vue
@@ -1,54 +1,64 @@
+<script>
+import axios from 'axios';
+import { LGeoJson, LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+
+require('../../node_modules/leaflet/dist/leaflet.css');
+
+export default {
+  name: 'CsMap',
+  components: {
+    LGeoJson,
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data() {
+    return {
+      map: {
+        bounds: null,
+        center: [50, 19],
+        data: null,
+        tileLayer: 'http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png',
+        zoom: 8,
+      },
+      trackId: '20180106-fatra',
+    };
+  },
+  methods: {
+    addTrack(trackData) {
+      this.map.data = trackData.data;
+    },
+    getTrack(id) {
+      return axios.get(`src/data/tracks/track-${id}.json`);
+    },
+  },
+  async mounted() {
+    try {
+      const track = await this.getTrack(this.trackId);
+      this.addTrack(track);
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e);
+    }
+
+    this.$nextTick(() => {
+      this.map.bounds = this.$refs.csdata.getBounds();
+      this.$refs.csmap.mapObject.fitBounds(this.map.bounds);
+    });
+  },
+};
+</script>
+
 <template>
   <div class="cs-map">
-    <div id="cs-map-container"></div>
+    <div id="cs-map-container">
+      <l-map :center="map.center" :zoom="map.zoom" ref="csmap">
+        <l-tile-layer :url="map.tileLayer" :center="map.center" :zoom="map.zoom" />
+        <l-geo-json v-if="map.data" :geojson="map.data" ref="csdata" />
+      </l-map>
+    </div>
   </div>
 </template>
-
-<script>
-  import L from 'leaflet';
-  import axios from 'axios';
-
-  require('../../node_modules/leaflet/dist/leaflet.css');
-
-  let map;
-
-  function addTrack(trackData) {
-    const geojson = trackData.data;
-    const trackLayer = L.geoJson(geojson);
-
-    map.addLayer(trackLayer);
-    map.fitBounds(trackLayer.getBounds());
-  }
-
-  function getTrack(id) {
-    return axios.get(`src/data/tracks/track-${id}.json`);
-  }
-
-  function initMap() {
-    const tileLayer = L.tileLayer(
-      'http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png',
-      {
-        subdomains: ['a', 'b', 'c'],
-        continuousWorld: true,
-      },
-    );
-    tileLayer.addTo(map);
-
-    getTrack('20180106-fatra').then(addTrack);
-  }
-
-  function createMap() {
-    map = L.map('cs-map-container');
-    map.on('load', () => initMap(map));
-    map.setView([50, 19], 8);
-  }
-
-  export default {
-    mounted() {
-      createMap();
-    },
-  };
-</script>
 
 <style>
   #cs-map-container {
