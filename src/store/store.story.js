62f1613ec3842ef7db4ab0d9c14d6efd0b282e48
Michal Zimmermann <zimmicz@gmail.com>
2019-09-21 19:48:12 +0200

    Fix store code structure

--- src/store/store.story.js
+++ src/store/store.story.js
@@ -2,35 +2,40 @@ import axios from 'axios';
 
 import { setPath } from './store.helpers';
 
-const storyModule = {
-  namespaced: true,
-  state: {
-    data: {
-      url: null,
-      story: null,
-    },
-  },
-  mutations: {
-    setStory: setPath(['data', 'story']),
-  },
-  actions: {
-    setHighlightedLink({ commit }) {
-      commit('setHighlightedLink', undefined, { root: true });
-    },
-    resetHighlightedLink({ commit }) {
-      commit('resetHighlightedLink', undefined, { root: true });
-    },
-    async loadText({ commit, state }) {
-      try {
-        const story = await axios.get(state.data.url);
-        commit('setStory', story.data);
-      } catch (e) {
-        // eslint-disable-next-line no-console
-        console.log(e);
-      }
-    },
+const state = {
+  data: {
+    url: undefined,
+    story: undefined,
   },
 };
 
-export default storyModule;
+const actions = {
+  setHighlightedLink({ commit }) {
+    commit('setHighlightedLink', undefined, { root: true });
+  },
+  resetHighlightedLink({ commit }) {
+    commit('resetHighlightedLink', undefined, { root: true });
+  },
+  async loadText({ commit, state }) {
+    try {
+      const story = await axios.get(state.data.url);
+      commit('setStory', story.data);
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e);
+    }
+  },
+};
 
+const mutations = {
+  setStory: setPath(['data', 'story']),
+};
+
+const storyModule = {
+  namespaced: true,
+  state,
+  actions,
+  mutations,
+};
+
+export default storyModule;
