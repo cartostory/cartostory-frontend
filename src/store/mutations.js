3cd417b668c6af3c5ea0cc6df5d817681ac3ec77
Michal Zimmermann <zimmicz@gmail.com>
2020-03-25 14:52:43 +0100

    Use mutation constants

---
+++ src/store/mutations.js
@@ -0,0 +1,15 @@
+export const RESET_HIGHLIGHTED_LINK = 'RESET_HIGHLIGHTED_LINK';
+export const SET_BBOX = 'SET_BBOX';
+export const SET_BBOX_HOVERED = 'SET_BBOX_HOVERED';
+export const SET_FEATURES_URL = 'SET_FEATURES_URL';
+export const SET_HIGHLIGHTED_FEATURE = 'SET_HIGHLIGHTED_FEATURE';
+export const SET_SHOULD_SCROLL_TO_FEATURE = 'SET_SHOULD_SCROLL_TO_FEATURE';
+export const SET_STORY_NAME = 'SET_STORY_NAME';
+export const SET_STORY_URL = 'SET_STORY_URL';
+export const SET_TRACK_URL = 'SET_TRACK_URL';
+
+// story mutations
+export const SET_STORY = 'SET_STORY';
+
+// track mutations
+export const SET_TRACK = 'SET_TRACK';
