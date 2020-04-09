5ca1588b6186df044aaa22a2eb39168a959cde6d
Michal Zimmermann <zimmicz@gmail.com>
2020-04-09 20:37:32 +0200

    Change routes

--- src/router.js
+++ src/router.js
@@ -1,16 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import CsLoadStoryForm from './components/CsLoadStoryForm.vue';
-import CsCreateStoryForm from './components/CsCreateStoryForm.vue';
-import CsScreen from './components/CsScreen.vue';
 import StoryScreen from '@/views/StoryScreen.vue';
 
 Vue.use(Router);
 
 export const routes = [
   { path: '/', component: StoryScreen, },
-  { path: '/load', component: CsLoadStoryForm },
-  { path: '/create', component: StoryScreen },
+  { path: '/story/load', component: CsLoadStoryForm },
+  { path: '/story/create', component: StoryScreen },
 ];
 
 export default new Router({
