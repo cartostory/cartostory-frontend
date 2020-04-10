74311313872387e48f07438533e486dc93fcf0b8
Michal Zimmermann <zimmicz@gmail.com>
2020-04-10 18:08:05 +0200

    Add map tests

---
+++ tests/unit/CsMap.spec.js
@@ -0,0 +1,53 @@
+import ElementUI from 'element-ui';
+import { createLocalVue, mount } from '@vue/test-utils';
+
+import CsMap from '@/components/CsMap.vue';
+import { STORY_LINK_LAT_ATTR, STORY_LINK_LNG_ATTR } from '@/config/config';
+import { UPDATE_FEATURE_MARK_CALLBACK, UPDATE_TRACK } from '@/store/mutations';
+
+const localVue = createLocalVue();
+localVue.use(ElementUI);
+
+describe('CsMap', () => {
+  test('sets track to store', () => {
+    const wrapper = mount(CsMap, {
+      mocks: {
+        $store: {
+          getters: {},
+          state: {
+            map: {},
+            story: {},
+          },
+          commit: jest.fn(),
+        },
+      },
+      localVue,
+    });
+
+    wrapper.vm.handleFileUpload({file: 'data'});
+    expect(wrapper.vm.$store.commit).toHaveBeenCalledTimes(1);
+    expect(wrapper.vm.$store.commit).toHaveBeenCalledWith(UPDATE_TRACK, {file: 'data'});
+  });
+
+  test('adds feature on map click', () => {
+    const wrapper = mount(CsMap, {
+      mocks: {
+        $store: {
+          getters: {},
+          state: {
+            addFeatureMarkCallback: jest.fn(),
+            map: {},
+            story: {},
+          },
+          commit: jest.fn(),
+        },
+      },
+      localVue,
+    });
+    wrapper.vm.handleMapClick({lat: 0, lng: 10});
+    expect(wrapper.vm.$store.state.addFeatureMarkCallback).toHaveBeenCalledTimes(1);
+    expect(wrapper.vm.$store.state.addFeatureMarkCallback).toHaveBeenCalledWith({[STORY_LINK_LAT_ATTR]: 0, [STORY_LINK_LNG_ATTR]: 10});
+    expect(wrapper.vm.$store.commit).toHaveBeenCalledTimes(1);
+    expect(wrapper.vm.$store.commit).toHaveBeenCalledWith(UPDATE_FEATURE_MARK_CALLBACK, undefined);
+  });
+});
