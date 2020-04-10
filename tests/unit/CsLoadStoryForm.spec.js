a5a74f7e191a792e9e1087e891fc958bf2f0137f
Michal Zimmermann <zimmicz@gmail.com>
2020-04-10 07:54:12 +0200

    Remove store from tests

--- tests/unit/CsLoadStoryForm.spec.js
+++ tests/unit/CsLoadStoryForm.spec.js
@@ -5,7 +5,7 @@ import { mount, createLocalVue } from '@vue/test-utils';
 
 import { UPDATE_STORY_URL } from '@/store/mutations';
 import CsLoadStoryForm from '@/components/CsLoadStoryForm.vue';
-import { defaultUrl, defaultStoryName, defaultStoryUrls } from '../helpers/data';
+import { defaultUrl } from '../helpers/data';
 import { routes } from '@/router';
 
 const localVue = createLocalVue();
@@ -15,49 +15,43 @@ localVue.use(Vuex);
 localVue.use(VueRouter);
 
 describe('CsLoadStoryForm.vue', () => {
-  test.only('renders a form without story select when no stories are available', () => {
-    const store = new Vuex.Store({
-      state: {
-        availableStories: [],
-      },
-    });
+  test('renders a form without story select when no stories are available', () => {
     const wrapper = mount(CsLoadStoryForm, {
-      store,
+      computed: {
+        availableStories: () => [],
+        storyUrl: () => defaultUrl,
+      },
       localVue,
     });
 
     expect(wrapper.findAll(FormItem).length).toEqual(2);
   });
-  test.only('renders a form with story select where at least one story is available', () => {
-    const store = new Vuex.Store({
-      state: {
-        availableStories: [{...defaultStoryUrls, storyName: defaultStoryName}],
-      },
-    });
 
+  test('renders a form with story select where at least one story is available', () => {
     const wrapper = mount(CsLoadStoryForm, {
-      store,
+      computed: {
+        availableStories: () => [defaultUrl],
+        storyUrl: () => defaultUrl,
+      },
       localVue,
     });
 
     expect(wrapper.findAll('.el-form-item').length).toEqual(3); // wrapper.findAll(FormItem) returns _FormItem is not defined
   });
-  test('sets input values when a story is selected', async () => {
-    const store = new Vuex.Store({
-      state: {
-        availableStories: [{...defaultStoryUrls, storyName: defaultStoryName}],
-      },
-    });
 
+  test('sets input values when a story is selected', async () => {
     const wrapper = mount(CsLoadStoryForm, {
-      store,
+      computed: {
+        availableStories: () => [defaultUrl],
+        storyUrl: () => defaultUrl,
+      },
       localVue,
     });
 
     const option = wrapper.get('.el-select-dropdown__item');
     option.vm.$el.dispatchEvent(new Event('click'));
     await wrapper.vm.$nextTick();
-    expect(wrapper.vm.storyUrl).toEqual(defaultStoryUrls.storyUrl);
+    expect(wrapper.vm.storyUrl).toEqual(defaultUrl);
   });
 
   test.only('loads the story from the given url', async () => {
